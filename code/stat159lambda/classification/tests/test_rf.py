1005e210bce3b0525f8b1362256135c89bb2cac4
Jordeen Chang <jordeenchang@gmail.com>
2015-12-10 17:38:16 -0800

    checking rf calls with right parameters

--- code/stat159lambda/classification/tests/test_rf.py
+++ code/stat159lambda/classification/tests/test_rf.py
@@ -5,18 +5,22 @@ try:
     from mock import patch
 except:
     from unittest.mock import patch
+from numpy.testing import assert_almost_equal, assert_array_equal
 
 
 def test_prepare():
     X = np.array([[10, 8, 8], [1, 11, 8], [4, 1, 1], [5, 0, 3], [7, 9, 1],
                   [1, 5, 8], [6, 3, 0], [7, 10, 3], [5, 4, 3], [7, 0, 8]])
     y = np.array([1, 0, 1, 1, 0, 2, 1, 1, 0, 0])
+
     return X, y
 
-# @patch.object(RandomForestClassifier, '__init__')
-# def test_rf_accuracy(mock_rf_init):
-#     X, y = test_prepare()
-#     rf.rf_accuracy(X, y, 2, 2, 2, num_folds=5)
-#     assert_array_equal(mock_rf_init.call_args[0][0],
-#                        np.array([[10, 8, 8], [1, 11, 8], [4, 1, 1], [5, 0, 3], [7, 9, 1],
-#                   [1, 5, 8], [6, 3, 0], [7, 10, 3], [5, 4, 3], [7, 0, 8]]))
+
+@patch.object(RandomForestClassifier, '__init__')
+def test_rf(mock_rf_init):
+    X, y = test_prepare()
+    mock_rf_init.return_value = None
+    test_classifier = rf.Classifier(X, y, depth=2)
+    mock_rf_init.assert_called_with(max_depth=None, max_features='auto', n_estimators=400, n_jobs=-1, oob_score=False)
+    test_classifier.train()
+    assert test_classifier.predict(X) is not None
